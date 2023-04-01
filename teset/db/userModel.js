f56cddd3ea978802a7e3798c4aafc8a1763b83a7
Hosam Rashed <hosam.wrr1234@gmail.com>
2023-04-01 19:11:48 +0300

    moving backend files to another folder

---
+++ teset/db/userModel.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+// user schema
+const UserSchema = new mongoose.Schema({
+  User_ID: {
+    type: String,
+    required: [true, "Please provide User ID!"],
+    unique: [true, "User_ID Exist"],
+  },
+
+  password: {
+    type: String,
+    required: [true, "Please provide a password!"],
+    unique: false,
+  },
+});
+
+// export UserSchema
+module.exports = mongoose.model.Users || mongoose.model("Users", UserSchema);
